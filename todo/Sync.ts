29d6046e41fde56039dde211db0bd86c9c27f334
Joshua Ohlman <ohlman@hey.com>
2021-06-12 08:54:50 -0400

    Notes about sync algorithm

---
+++ todo/Sync.ts
@@ -0,0 +1,6 @@
+export class Sync {
+  // keeps track of server state
+  // keeps track of local state
+  // sends updates to the bus whenever they are received from the server
+  // sends updates to the server periodically (e.g. 1 per second when there are any)
+}
--- todo/Update.ts
+++ todo/Update.ts
@@ -4,6 +4,7 @@ import Revision from "./Revision";
 export default class Update {
   constructor(
     replacedEvents: string[],
+    replacementPoint: string,
     replacementEvents: (Mutation | Revision)[]
   ) {}
 }
