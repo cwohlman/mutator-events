4bbf59b9751e7ab938d505891f5e481d217b9d20
Joshua Ohlman <ohlman@hey.com>
2021-06-15 09:56:04 -0400

    Spec for insertAfter

--- todo/Mutables/Ordered.spec.ts
+++ todo/Mutables/Ordered.spec.ts
@@ -1,17 +1,28 @@
 import { Map, List } from "immutable"
+import { InsertAfter } from "../Mutations/InsertAfter"
 import { Box } from "./Box"
 import { Ordered } from "./Ordered"
 import { Ref } from "./Ref"
 
-describe("Hash", () => {
+describe("ordered", () => {
   it("should return an object", () => {
-    const hash = new Ordered(Map({ "1": new Box("5") }), List(["1"]));
+    const ordered = new Ordered(Map({ "1": new Box("5") }), List(["1"]));
 
-    expect(hash.toJS(null)).toEqual(["5"]);
+    expect(ordered.toJS(null)).toEqual(["5"]);
   })
   it("should pass context to children", () => {
-    const hash = new Ordered(Map({ "1": new Ref("5") }), List(["1"]));
+    const ordered = new Ordered(Map({ "1": new Ref("5") }), List(["1"]));
 
-    expect(hash.toJS({ getRef() { return "99" }})).toEqual(["99"]);
+    expect(ordered.toJS({ getRef() { return "99" }})).toEqual(["99"]);
   })
+  describe("InsertAfter", () => {
+    it("should set the key", () => {
+    const ordered = new Ordered(Map({ "1": new Box("5") }), List(["1"]));
+      const change = new InsertAfter("1", [], "99", "1", new Box("99"));
+
+      const newOrdered = ordered.apply(change);
+
+      expect(newOrdered.toJS(null)).toEqual(["5", "99"]);
+    });
+  });
 })
